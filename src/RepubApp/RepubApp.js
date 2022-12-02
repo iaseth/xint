4b1ffd6d442b0f67c0b76cf44cb56ca97f3d6538
Ankur Seth <iankurseth@gmail.com>
2022-12-02 22:04:37 +0530

    Tailwind, ESLint, NY and cleanup * Added root RepubApp component

---
+++ src/RepubApp/RepubApp.js
@@ -0,0 +1,13 @@
+import './RepubApp.scss';
+
+
+
+export default function RepubApp () {
+	return (
+		<div>
+			<header className="min-h-screen bg-red-500 text-white flex">
+				<h1 className="text-4xl m-auto">RepubApp</h1>
+			</header>
+		</div>
+	);
+}
